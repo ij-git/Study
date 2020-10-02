8cbed182622579deba1eaf34f8f4b68a7a858a1a
ij-git <ijeong.dev@gmail.com>
2020-10-02 21:02:15 +0900

    update error log and gitignore

--- node.js/4.module/4.module(4).internal_module.js
+++ node.js/4.module/4.module(4).internal_module.js
@@ -10,3 +10,22 @@ console.dir(os.cpus());
 
 console.log(`system network interface info:`);
 console.dir(os.networkInterfaces());
+
+
+
+
+var path = require('path');
+
+var directories = ["users", "ijeong", "docs"];
+var docsDirectory = directories.join(path.sep);
+console.log('docs dir: %s', docsDirectory);
+
+var curPath = path.join('/User/ijoeng', 'notepad.exe');
+console.log('filepath: %s', curPath);
+
+var filename = 'C:\\Users\\ijeong\\notepad.exe';
+var dirname = path.dirname(filename);
+var basename = path.basename(filename);
+var extname = path.extname(filename);
+
+console.log(`directory: ${dirname}', fliename: ${basename}', 확장자: ${extname}`);
